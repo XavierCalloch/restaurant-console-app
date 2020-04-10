879705139ea1ede81b9448c466932bac0ca7b9b3
Xavier CALLOCH <callochxavier@gmail.com>
2020-04-10 11:26:15 +0200

    6e commit - TP 3.3 Spring Test

---
+++ src/test/java/dev/service/PlatServiceVersion2IntegrationTest.java
@@ -0,0 +1,39 @@
+package dev.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import dev.dao.PlatDaoMemoire;
+
+@SpringJUnitConfig({ PlatServiceVersion2.class, PlatDaoMemoire.class })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles({ "service2", "memoire" })
+public class PlatServiceVersion2IntegrationTest {
+
+	// injection du bean à tester
+	@Autowired
+	PlatServiceVersion2 platServiceVersion2;
+	// PlatDaoMemoire platDaoMemoire;
+
+	@Test
+	void testAjouterPlatCasPassant() {
+		platServiceVersion2.ajouterPlat("Riz cantonais", 1100);
+
+		assertFalse(platServiceVersion2.listerPlats().isEmpty());
+	}
+
+	@Test
+	void testAjouterPlatPrixInvalide() {
+
+		assertThatThrownBy(() -> {
+			platServiceVersion2.ajouterPlat("Bol renversé", 500);
+		}).hasMessage("le prix d'un plat doit être supérieur à 10 €");
+	}
+
+}
